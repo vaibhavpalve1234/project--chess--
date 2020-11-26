26e18f4c0deac656b9a7cdbf860b7d31f51173fb
vaibhav <vaibhavpalve1234@gmail.com>
2020-11-26 15:51:55 +0530

    chess_project

--- chessmain.py
+++ chessmain.py
@@ -1,6 +1,7 @@
 # from python import * 
 import pygame as p
 import chessEngine
+import turtle
 
 WIDTH = HEIGHT = 512
 
@@ -169,78 +170,6 @@ def drawText(screen,text):
 
 
 
-# def draw_board(dimension, x_coord, y_coord, side):
 
-#     parity = False
-
-#     for i in range(dimension ** 2):
-#         if i % dimension == 0:
-#             y_coord -= side
-#             turtle.penup()
-#             turtle.setpos(x_coord, y_coord)
-#             turtle.pendown()
-#             parity = parity != (dimension % 2 == 0)  # logical XOR
-
-#         if parity:
-#             turtle.begin_fill()
-
-#         for _ in range(4):
-#             turtle.forward(side)
-#             turtle.right(90)
-
-#         if turtle.filling():
-#             turtle.end_fill()
-
-#         turtle.forward(side)
-
-#         parity = not parity
-
-
-
-if __name__ == '__main__':
-    
-    # size = 8
-
-    # turtle.speed('fastest')
-
-    # turtle.pensize(5)
-
-    # draw_board(size, -450, 500, 100)
-
-    # turtle.hideturtle()
-
-    # turtle.exitonclick()
-    # chessboard = turtle.Turtle()
-    # chessboard.speed(8) #for speedily drawing chessboard
-    # for i in range(4): #for loop will run 4 times
-    #     chessboard.forward(800) #forward turtle by 800 units
-    #     chessboard.right(90) #turn turtle clockwise by 90 degree
-    # a = 0 #for controlling alternate colors in a row
-    # b = 0 #for controlling alternate colors in a column
-    # for i in range(8): #for loop will run 8 times as there are 8 rows in the chessboard
-    #     if(b == 0):
-    #         a=1
-    #     else:
-    #         a=0
-    #     for j in range(8): #for loop will run 8 times as there are 8 columns in the chessboard
-    #         chessboard.penup()
-    #         chessboard.goto(j*100, i*100*(-1))
-    #         chessboard.pendown()
-    #         if(a==0):
-    #             chessboard.fillcolor('black')
-    #             a=1
-    #         else:
-    #             chessboard.fillcolor('white')
-    #             a=0
-    #         chessboard.begin_fill()
-    #         for k in range(4):
-    #             chessboard.forward(100)
-    #             chessboard.right(90)
-    #         chessboard.end_fill()
-    #     if(b==0):
-    #         b=1
-    #     else:
-    #         b=0
-
-    main()
+main()
     
